a6930946a0ab5edc6f904b54ce0385228ef6149f
Sơn <sonngoc.gg@gmail.com>
2021-07-04 11:32:02 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# I'm using this repository to learn a library called _RxJS_
+
+Feel free to using any lines of code in this repository
+
+## Scripts
+
+### Build
+
+_This script is to build the project into production ready_
+
+### Watch
+
+_This script is to watch the change in source code and recompiled, so you don't have to run full build manually_
+
+### Start
+
+_This script is to start the development server_
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+	entry: './src/index.ts',
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist',
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+			filename: './index.html',
+		}),
+	],
+}
